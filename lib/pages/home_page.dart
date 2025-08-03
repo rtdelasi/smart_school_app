7d075ec6715974c99480a7a08285a17e1f6c9a35
rtdelasi <rdtamakloe1@gmail.com>
2025-08-03 23:00:11 +0000

    Add Google Navigation Bar and Badges dependencies; update HomePage layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:smart_school_app/pages/cart_page.dart';
 import 'package:smart_school_app/pages/explore_page.dart';
 import 'package:smart_school_app/pages/favourites_page.dart';
@@ -12,39 +13,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _currentIndex = 0;
+  int _selectedIndex = 0;
 
-  final List<Widget> _pages = [
-    const ExplorePage(), // You should create a separate widget for the home content
-    const CartPage(),
-    const FavouritesPage(),
-    const ProfilePage(),
+  final List _pages = [
+    ExplorePage(),
+    CartPage(),
+    FavouritesPage(),
+    ProfilePage(),
   ];
-
-  void _navigateBottomBar(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(),
-      appBar: AppBar(),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _navigateBottomBar,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: "Cart",
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 12),
+          child: GNav(
+            backgroundColor: Colors.white,
+            color: Colors.grey[600],
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.blue.shade800,
+            gap: 8,
+            padding: const EdgeInsets.all(12),
+            tabs: const [
+              GButton(icon: Icons.home, text: 'Home'),
+              GButton(icon: Icons.favorite_border, text: 'Likes'),
+              GButton(icon: Icons.search, text: 'Search'),
+              GButton(icon: Icons.person, text: 'Profile'),
+            ],
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favourites"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-        ],
+        ),
       ),
     );
   }
